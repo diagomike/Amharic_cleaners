51dbdb7295ddc86501f37bbedc17ca0a7b0b6b66
Diago <diagomike199@gmail.com>
2023-04-05 23:42:26 -0900

    Data loaded

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-from prep import amh_prep
+from prep import amh_prep,phoneme_prep,clean_phoneme_prep
+
 
 amh_prep('format/train_list.txt','clean_train_list.txt')
 amh_prep('format/val_list.txt','clean_val_list.txt')
+
+phoneme_prep('format/train_list.txt','pho_train_list.txt')
+phoneme_prep('format/val_list.txt','pho_val_list.txt')
+
+clean_phoneme_prep('format/train_list.txt','pho_cl_train_list.txt')
+clean_phoneme_prep('format/val_list.txt','pho_cl_val_list.txt')
