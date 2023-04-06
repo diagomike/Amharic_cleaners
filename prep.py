71a92f5f40bd87d323ab78dbeda0d7b969a828e1
Diago <diagomike199@gmail.com>
2023-04-05 23:16:33 -0900

    clean and phonemize

---
+++ prep.py
@@ -0,0 +1,33 @@
+from DataPreprocessor.cleaner import amharic_cleaners
+# load phonemizer
+# import phonemizer
+# global_phonemizer = phonemizer.backend.EspeakBackend(language='am', 
+#                                                      preserve_punctuation=True,  
+#                                                      with_stress=True, 
+#                                                      language_switch='remove-flags',
+#                                                      words_mismatch='ignore')
+
+
+# def phoneme_prep(input_path:str,out_path):
+#     # input_path /content/drive/MyDrive/ASR/item_list.txt'
+#     # out_path /content/drive/MyDrive/ASR/item_IPA_list.txt'
+#     item_list = open(input_path,'r',encoding='utf8').read()
+#     item_new = []
+#     item_texts = item_list.splitlines()
+#     for text in item_texts:
+#         writable = text.split('|')
+#         writable[1] = global_phonemizer.phonemize([writable[1]])[0]
+#         item_new.append('|'.join(writable))
+#     open(out_path,'w',encoding='utf8').write('\n'.join(item_new))
+
+def amh_prep(input_path:str,out_path):
+    # input_path /content/drive/MyDrive/ASR/item_list.txt'
+    # out_path /content/drive/MyDrive/ASR/item_IPA_list.txt'
+    item_list = open(input_path,'r',encoding='utf8').read()
+    item_new = []
+    item_texts = item_list.splitlines()
+    for text in item_texts:
+        writable = text.split('|')
+        writable[1] = amharic_cleaners(writable[1])
+        item_new.append('|'.join(writable))
+    open(out_path,'w',encoding='utf8').write('\n'.join(item_new))
---
+++ test.py
@@ -0,0 +1,4 @@
+from prep import amh_prep
+
+amh_prep('format/train_list.txt','clean_train_list.txt')
+amh_prep('format/val_list.txt','clean_val_list.txt')
